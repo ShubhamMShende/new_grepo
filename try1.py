88c2d89f36307ad5abdd769fb0f8870ef7a1dc40
Shubham <shendeshubham987@gmail.com>
2021-09-13 23:16:26 +0530

    added try.py

---
+++ try1.py
@@ -0,0 +1,7 @@
+n = int(input('\nType a number, and its factorial will be printed: '))
+if n < 0:
+    raise ValueError('You must enter a non negative integer')
+fact = 1
+for i in range(2, n + 1):
+    fact = fact * i
+print('\nFactorial of',n,'is',fact)
