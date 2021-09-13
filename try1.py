632f06459d052b3bd9b414dbb156e040af03f3fb
Shubham <shendeshubham987@gmail.com>
2021-09-13 23:19:52 +0530

    first change

--- try1.py
+++ try1.py
@@ -1,6 +1,6 @@
 n = int(input('\nType a number, and its factorial will be printed: '))
 if n < 0:
-    raise ValueError('You must enter a non negative integer')
+    print('You must enter a non negative integer')
 fact = 1
 for i in range(2, n + 1):
     fact = fact * i
